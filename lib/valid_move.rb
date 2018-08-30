eef7190e31ac1e2a9023959aaa5279d546e7a965
Min Hee  Park <minheekangg@gmail.com>
2018-08-30 00:39:24 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?
-  if position_taken? == false && index.between?(0,8) == true
+  if position_taken? && index.between?(0,8)
     return true
   else
     return false
