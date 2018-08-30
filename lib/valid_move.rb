02803929fcf1fa6dc85c4a121eb925dc0fd699cd
Min Hee  Park <minheekangg@gmail.com>
2018-08-30 00:59:01 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move? (board, index)
   if  index.between?(0, 8)
     return true
-  elsif  position_taken? == true
+  elsif position_taken? == true
     return false
   else
     return false
